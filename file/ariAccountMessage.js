f9d848cf64a7f97aec8489bba77351bb7ee17c04
abr <9946549@qq.com>
2025-06-28 17:34:02 +0800

    Update ariAccountMessage.js

--- file/ariAccountMessage.js
+++ file/ariAccountMessage.js
@@ -1,2 +1,10 @@
 $(function(){
+  var promise = new Promise((resolveOuter) => { resolveOuter(new Promise((resolveInner) => {setTimeout(resolveInner, 10);}),);});
+  promise.then(()=>{
+    
+  })
+  promise.then(()=>{
+    $('div#animations').fadeOut(500);
+    $('div#relcontent').fadeIn(500);
+  })
 });
